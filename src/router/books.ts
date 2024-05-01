53f9db0a22ce6f70d8ad1e14be781f811cef35f3
MJ van Staden <mjvstaden01@gmail.com>
2024-05-01 19:31:29 +0200

    Create book input validation

--- src/router/books.ts
+++ src/router/books.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import { getAllBooks, addNewBook, getBook, updateBook, getDiscountedPrice, deleteBook} from '../controllers/books';
 
 export default (router: express.Router) => {
     router.get('/books', getAllBooks);
-    router.post('/book/create', addNewBook);
+    router.post('/book/create',
+        body('title').isString().notEmpty(),
+        body('author').isString().notEmpty(),
+        body('genre').isString().notEmpty(),
+        body('price').isNumeric().notEmpty(),
+        async (req: express.Request, res: express.Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            addNewBook(req, res);
+        }
+    );
     router.get('/book/find/:id', getBook);
     router.put('/book/update/:id', updateBook);
     router.get('/books/discounted-price', getDiscountedPrice);
