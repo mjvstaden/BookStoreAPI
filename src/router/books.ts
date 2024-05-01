1947c0fc8255ec6f4fbde11522e98b0b912fb24a
MJ van Staden <mjvstaden01@gmail.com>
2024-05-01 19:40:24 +0200

    router input validation for all routes

--- src/router/books.ts
+++ src/router/books.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
+import { body, param, query, validationResult } from 'express-validator';
 import { getAllBooks, addNewBook, getBook, updateBook, getDiscountedPrice, deleteBook} from '../controllers/books';
 
 export default (router: express.Router) => {
@@ -9,16 +9,53 @@ export default (router: express.Router) => {
         body('author').isString().notEmpty(),
         body('genre').isString().notEmpty(),
         body('price').isNumeric().notEmpty(),
+            async (req: express.Request, res: express.Response) => {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({ errors: errors.array() });
+                }
+                addNewBook(req, res);
+            }
+        );
+    router.get('/book/find/:id',
+        param('id').isMongoId(),
         async (req: express.Request, res: express.Response) => {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
             }
-            addNewBook(req, res);
+            getBook(req, res);
         }
     );
-    router.get('/book/find/:id', getBook);
-    router.put('/book/update/:id', updateBook);
-    router.get('/books/discounted-price', getDiscountedPrice);
-    router.delete('/book/:id', deleteBook);
+    router.put('/book/update/:id',
+        param('id').isMongoId(),
+        async (req: express.Request, res: express.Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            updateBook(req, res);
+        }
+    );
+    router.get('/books/discounted-price',
+        query('genre').isString().notEmpty(),
+        query('discount').isNumeric().notEmpty(),
+            async (req: express.Request, res: express.Response) => {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({ errors: errors.array() });
+                }
+                getDiscountedPrice(req, res);
+            }
+        );
+    router.delete('/book/:id', 
+        param('id').isMongoId(),
+        async (req: express.Request, res: express.Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            deleteBook(req, res);
+        }
+    );
 }
